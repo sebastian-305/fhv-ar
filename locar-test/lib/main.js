1ed3ecdd77aee17b3e31919e3635cc01cb1f8822
Sebastian Bogensperger <106682134+sebastian-305@users.noreply.github.com>
2024-12-04 17:06:08 +0100

    Revert "test-upload"
    This reverts commit 0aa7fcf397044ebf4674ad17abb6ba4ad9303b98.

--- locar-test/lib/main.js
+++
@@ -1,6 +0,0 @@
-import { LocationBased } from "./src/location-based.js";
-import { WebcamRenderer } from "./src/webcam-renderer.js";
-import { DeviceOrientationControls } from "./src/device-orientation-controls.js";
-import { ClickHandler } from './src/click-handler.js';
-
-export { LocationBased, WebcamRenderer, DeviceOrientationControls, ClickHandler  };
